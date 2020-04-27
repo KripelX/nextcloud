16f3a3dc1e53ebe33aece84b69a53d9bc45852e2
Andreas Kripel <andreas.kripel@gmail.com>
2020-04-27 16:39:55 +0200

    neue Datei:     config.php 	geändert:       docker-compose.yml

---
+++ config.php
@@ -0,0 +1,53 @@
+<?php
+$CONFIG = array (
+  'htaccess.RewriteBase' => '/',
+  'memcache.local' => '\\OC\\Memcache\\APCu',
+  'apps_paths' =>
+  array (
+    0 =>
+    array (
+      'path' => '/var/www/html/apps',
+      'url' => '/apps',
+      'writable' => false,
+    ),
+    1 =>
+    array (
+      'path' => '/var/www/html/custom_apps',
+      'url' => '/custom_apps',
+      'writable' => true,
+    ),
+  ),
+  'instanceid' => 'ock9l3onk5m8',
+  'passwordsalt' => 'TqErhM216ZUIuL7vHMy/HnjLb97vEi',
+  'secret' => 'sgBrKaf+alR4XlwFQYu2wFuftws6RHLk66Iyg/yIjZ/bLUF+',
+  'trusted_domains' =>
+  array (
+    0 => '10.0.0.97',
+    1 => 'nextcloud.kripel',
+    2 => 'localhost',
+    3 => '172.18.0.4',
+  ),
+  'datadirectory' => '/var/www/html/data',
+  'dbtype' => 'pgsql',
+  'version' => '18.0.4.2',
+  'overwrite.cli.url' => 'http://nextcloud.kripel',
+  'dbname' => 'nextcloud',
+  'dbhost' => '10.0.0.97:5432',
+  'dbport' => '',
+  'dbtableprefix' => 'oc_',
+  'dbuser' => 'oc_andik31',
+  'dbpassword' => 'mibgyxh1elrnxmlvd0pn1ecdhs3ogw',
+  'installed' => true,
+  'maintenance' => false,
+  'loglevel' => 2,
+  'overwritehost' => 'nextcloud.kripel',
+  'overwriteprotocol' => 'https',
+  'forwarded_for_headers' =>
+  array (
+            0 => 'HTTP_X_FORWARDED_FOR',
+    ),
+  'trusted_proxies' =>
+  array (
+          0 => '172.18.0.2',
+    ),
+);

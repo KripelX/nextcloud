6cbb87db7dc64fd34bd2e7f005514823caf84cf8
andreas <andreas.kripel@live.com>
2021-03-18 19:45:40 +0100

    nuc7akr: server switch commit

--- config.php
+++ config.php
@@ -2,44 +2,42 @@
 $CONFIG = array (
   'htaccess.RewriteBase' => '/',
   'memcache.local' => '\\OC\\Memcache\\APCu',
-  'apps_paths' =>
+  'apps_paths' => 
   array (
-    0 =>
+    0 => 
     array (
       'path' => '/var/www/html/apps',
       'url' => '/apps',
       'writable' => false,
     ),
-    1 =>
+    1 => 
     array (
       'path' => '/var/www/html/custom_apps',
       'url' => '/custom_apps',
       'writable' => true,
     ),
   ),
-  'instanceid' => 'ock9l3onk5m8',
-  'passwordsalt' => 'TqErhM216ZUIuL7vHMy/HnjLb97vEi',
-  'secret' => 'sgBrKaf+alR4XlwFQYu2wFuftws6RHLk66Iyg/yIjZ/bLUF+',
-  'trusted_domains' =>
+  'instanceid' => 'oczbwian7w4z',
+  'passwordsalt' => 'rM1L7kB+n2xHOJ58kkmBJS6fZfiL44',
+  'secret' => 'lNO+PvxULwgCgQfO+zULu8yUglr31XrOf4I74UeJGUWsB4aO',
+  'trusted_domains' => 
   array (
-    0 => '10.0.0.97',
-    1 => 'nextcloud.dandi',
+    0 => 'nextcloud.dandi',
+    1 => '10.0.0.88',
     2 => 'localhost',
-    3 => '172.18.0.4',
   ),
   'datadirectory' => '/var/www/html/data',
   'dbtype' => 'pgsql',
-  'version' => '18.0.4.2',
+  'version' => '21.0.0.18',
   'overwrite.cli.url' => 'http://nextcloud.dandi',
   'dbname' => 'nextcloud',
-  'dbhost' => '10.0.0.97:5432',
+  'dbhost' => '10.0.0.88:5432',
   'dbport' => '',
   'dbtableprefix' => 'oc_',
   'dbuser' => 'oc_andik31',
-  'dbpassword' => 'mibgyxh1elrnxmlvd0pn1ecdhs3ogw',
+  'dbpassword' => 'bes3x2xpu4781t8dmgimevral7gw02',
   'installed' => true,
-  'maintenance' => false,
-  'loglevel' => 2,
+  'default_phone_region' => 'AT',
   'overwritehost' => 'nextcloud.dandi',
   'overwriteprotocol' => 'https',
   'forwarded_for_headers' =>
@@ -48,6 +46,6 @@ $CONFIG = array (
     ),
   'trusted_proxies' =>
   array (
-          0 => '172.18.0.2',
+          0 => '172.18.0.3',
     ),
 );
